6a2361f8c268a67f84b94936183b2c6dcbf46e69
Nicolo Maioli <nico.maioli@skinandme.com>
2026-05-05 15:47:45 +0100

    Configure mini.diff

--- nvim/.config/nvim/lua/plugins/mini-diff.lua
+++ nvim/.config/nvim/lua/plugins/mini-diff.lua
@@ -2,5 +2,18 @@ return {
 	"nvim-mini/mini.diff",
 	version = false,
 	event = { "BufReadPre", "BufNewFile" },
-	opts = {},
+	opts = {
+		view = {
+			style = "sign",
+		},
+	},
+	keys = {
+		{
+			"<leader>do",
+			function()
+				require("mini.diff").toggle_overlay(0)
+			end,
+			desc = "Toggle mini.diff overlay",
+		},
+	},
 }
