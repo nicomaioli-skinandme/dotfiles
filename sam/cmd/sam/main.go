c007bcf6cd94d814a342ff162bc66293445cf9c0
Nicolo Maioli <nico.maioli@skinandme.com>
2026-05-20 20:31:31 +0200

    link new internal packages from main and drop tools.go

--- sam/cmd/sam/main.go
+++ sam/cmd/sam/main.go
@@ -5,6 +5,11 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+
+	_ "github.com/nicomaioli-skinandme/dotfiles/sam/internal/ghx"
+	_ "github.com/nicomaioli-skinandme/dotfiles/sam/internal/gitx"
+	_ "github.com/nicomaioli-skinandme/dotfiles/sam/internal/tmuxx"
+	_ "github.com/nicomaioli-skinandme/dotfiles/sam/internal/ui"
 )
 
 var projectFlag string
