17f3548bfdb80da38f0981959809ac092c778d94
Nicolo Maioli <nico.maioli@skinandme.com>
2026-05-27 16:11:35 +0100

    Disambiguate <leader>gb

--- private_dot_config/nvim/lua/plugins/git.lua
+++ private_dot_config/nvim/lua/plugins/git.lua
@@ -23,8 +23,8 @@ return {
 			{ "<leader>gy", "<Cmd>.GBrowse!<CR>", desc = "Yank GitHub permalink" },
 			{ "<leader>gy", ":GBrowse!<CR>", mode = "v", desc = "Yank GitHub permalink" },
 
-			{ "<leader>gb", "<Cmd>GBrowse<CR>", desc = "Browse file on GitHub" },
-			{ "<leader>gb", ":GBrowse<CR>", mode = "v", desc = "Browse selection on GitHub" },
+			{ "<leader>gB", "<Cmd>GBrowse<CR>", desc = "Browse file on GitHub" },
+			{ "<leader>gB", ":GBrowse<CR>", mode = "v", desc = "Browse selection on GitHub" },
 
 			{ "<leader>gh", "<Cmd>!gh pr view --web<CR>", desc = "Open current PR on GitHub" },
 			{ "<leader>gpr", "<Cmd>!gh pr create --draft --web<CR>", desc = "Open draft PR on GitHub" },
